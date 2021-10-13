376353cb1882421df564ec89a19b2fb407e32a0d
joghub <jorgeoliverag@gmail.com>
2021-10-13 14:25:38 +0200

    primer subbloque de simulink añadido, no funcionando aun

---
+++ codigos.m
@@ -0,0 +1,24 @@
+m=15; %100 % Masa del misil
+g=9.80665; % Gravedad
+c = 0.2; 
+Long=2;
+h=0.7;
+b_ala=3;
+L_ala=0.25;
+b_est=1;
+L_est=0.1;
+e = 0.75;
+CL0=0.25;
+CLalpha=5.5;
+Cd0=0.045;
+Cl_p=0.51;
+Cl_deltaa=0.17;
+Cm_w=0.0005;
+Cm_fq=0.1;
+Cm_deltae=1;
+Cn_v=0.002;
+Cn_fr=0.1;
+Cn_deltar=0.07;
+
+
+
